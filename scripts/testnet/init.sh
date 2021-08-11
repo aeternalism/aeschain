8e9f87a3993119953c57ab4c9e87ad0e32b16680
lebrian <lebrain@aeternalism.com>
2021-08-11 23:35:37 +0700

    update testnet scripts

--- scripts/testnet/init.sh
+++ scripts/testnet/init.sh
@@ -7,7 +7,7 @@ MONIKER=$1
 aeschaind init ${MONIKER} --chain-id ${CHAIN_ID} --home ${APP_HOME}
 
 # override genesis
-# curl https://raw.githubusercontent.com/aeternalism/aeschain-version/main/hydrogen/testnet/genesis.json > ${APP_HOME}/config/genesis.json
+curl https://raw.githubusercontent.com/aeternalism/aeschain-version/main/oxygen/testnet/genesis.json > ${APP_HOME}/config/genesis.json
 
 # validate genesis
 aeschaind validate-genesis
--- scripts/testnet/start.sh
+++ scripts/testnet/start.sh
@@ -4,8 +4,7 @@ CHAIN_ID="aeternalism"
 MONIKER=$1
 
 # get seeds
-# SEEDS=$(curl https://raw.githubusercontent.com/aeternalism/aeschain-version/main/hydrogen/testnet/seeds)
-SEEDS=574fbc6543ab45c3559ea1ecb8e3e45576b93a72@52.220.154.234:26656
+SEEDS=$(curl https://raw.githubusercontent.com/aeternalism/aeschain-version/main/oxygen/testnet/seeds)
 
 # run the node
 aeschaind start \
